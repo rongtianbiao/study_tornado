5351dfea0337c44d5dfeff8a6a1e5a633623ca9b
rongtianbiao <921833709@qq.com>
2019-05-29 22:54:14 +0800

    first

---
+++ config.py
@@ -0,0 +1,13 @@
+# 数据库配置参数
+mysql_options = dict(
+    host="127.0.0.1",
+    database="ihome",
+    user="root",
+    password="3535"
+)
+
+# Redis配置参数
+redis_options = dict(
+    host="127.0.0.1",
+    port=6379
+)
