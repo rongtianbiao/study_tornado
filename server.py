80656dc3a9523d5a9de5770847f44db61b045207
rongtianbiao <921833709@qq.com>
2019-05-31 14:28:52 +0800

    修改server

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
 import tornado.ioloop
 import tornado.web
 from tornado.options import define, options, parse_command_line
+import tornado.httpserver
 
 define("port", default=8000, type=int)
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.name = "python"
+        print("实例对象的初始化方法，并给name参数赋值")
+
+    def prepare(self):
+        print("在执行HTTP行为方法之前被调用")
+
     def get(self, *args, **kwargs):
-        name = self.get_argument("name", "xiaoming")
-        self.write("hello %s"%name)
+        print("执行get方法")
+        self.write("hello %s"%self.name)
+
+    def on_finish(self):
+        print("响应后调用此方法")
 
 
 def make_app():
